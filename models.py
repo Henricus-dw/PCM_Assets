1177c8215cf261cc773520aee961433f617cf4e1
Henricus <dewitthenri@gmail.com>
2025-08-21 17:13:48 +0200

    Fixed dash board, added form

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ Base = declarative_base()
 class VodacomSubscription(Base):
     __tablename__ = "Vodacom_subscription"
 
-    # new primary key column
     id = Column(Integer, primary_key=True, autoincrement=True)
     Name_ = Column(String(50))
     Surname_ = Column(String(50))
@@ -16,6 +15,8 @@ class VodacomSubscription(Base):
     Client_Division = Column(String(100))
     Contract_Type = Column(String(50))
     Monthly_Costs = Column(Float)
+    VAT = Column(Float)  # ✅ Fixed
+    Monthly_Cost_Excl_VAT = Column(Float)  # ✅ Fixed
     Contract_Term = Column(String(50))
     Sim_Card_Number = Column(String(50))
     Inception_Date = Column(DateTime)
