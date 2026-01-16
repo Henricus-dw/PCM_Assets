12334e7470d771d2b57506434b06286dc6931305
Henricus <dewitthenri@gmail.com>
2026-01-16 11:29:58 +0200

    Add local SQLite support and ignore local artifacts

--- database.py
+++ database.py
@@ -1,7 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+mysqlconnector://pcm_user:Activate!2000@localhost/pcm_db"
+ENV = os.getenv("APP_ENV", "production")
+
+if ENV == "local":
+    # Local dev: plug-and-play DB file in the project folder
+    DATABASE_URL = "sqlite:///./local.db"
+
+    engine = create_engine(
+        DATABASE_URL,
+        # needed for SQLite + FastAPI
+        connect_args={"check_same_thread": False},
+    )
+else:
+    # Production: keep hard-coded MySQL exactly as before
+    DATABASE_URL = "mysql+mysqlconnector://pcm_user:Activate!2000@localhost/pcm_db"
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 
 engine = create_engine(DATABASE_URL)
